8ad0d4de890c058450ba9c55021a43cd56fa3c7f
sayeedajmal <sayeedajmala06@gmail.com>
2024-09-24 21:47:58 +0530

    added resume

--- sayeedcodes/schemaTypes/index.js
+++ sayeedcodes/schemaTypes/index.js
@@ -2,5 +2,6 @@ import about from './about'
 import about_picture from "./about_picture"
 import skills from "./skills"
 import works from "./works"
+import resume from "./resume"
 
-export const schemaTypes = [about, about_picture, skills, works]
+export const schemaTypes = [about, about_picture, skills, works,resume]
